e84d59bc077b87aeb6e3b0e543eabfff2723fe5b
davide\Administrator <davide.cremona@gmail.com>
2024-03-06 17:04:20 +0100

    fix job squal ind

--- src/main/java/fcweb/security/AuthenticatedUser.java
+++ src/main/java/fcweb/security/AuthenticatedUser.java
@@ -1,9 +1,5 @@
 package fcweb.security;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -19,6 +15,7 @@ import org.springframework.stereotype.Component;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.spring.security.AuthenticationContext;
 
+import common.util.Utils;
 import fcweb.backend.data.entity.FcAttore;
 import fcweb.backend.data.entity.FcCampionato;
 import fcweb.backend.data.entity.FcGiornataInfo;
@@ -116,11 +113,11 @@ public class AuthenticatedUser{
 		LOG.info("CurrentGG: " + giornataInfo.getCodiceGiornata());
 
 		String fusoOrario = (String) properties.getProperty("FUSO_ORARIO");
-		String nextDate = getNextDate(giornataInfo);
+		String nextDate = Utils.getNextDate(giornataInfo);
 
 		long millisDiff = 0;
 		try {
-			millisDiff = getMillisDiff(nextDate, fusoOrario);
+			millisDiff = Utils.getMillisDiff(nextDate, fusoOrario);
 		} catch (Exception e) {
 			LOG.error(e.getMessage());
 		}
@@ -137,57 +134,6 @@ public class AuthenticatedUser{
 		VaadinSession.getCurrent().setAttribute("COUNTDOWNDATE", getCalendarCountDown(nextDate, fusoOrario));
 
 		return true;
-	}
-
-	private String getNextDate(FcGiornataInfo giornataInfo) {
-
-		LocalDateTime now = LocalDateTime.now();
-		LocalDateTime currentDate = LocalDateTime.now();
-
-		LocalDateTime dataAnticipo = null;
-		LocalDateTime dataAnticipo1 = giornataInfo.getDataAnticipo1();
-		LocalDateTime dataAnticipo2 = giornataInfo.getDataAnticipo2();
-		if (dataAnticipo1 != null && dataAnticipo2 != null) {
-			if (now.getDayOfWeek() == dataAnticipo1.getDayOfWeek()) {
-				dataAnticipo = dataAnticipo1;
-			} else if (now.getDayOfWeek() == dataAnticipo2.getDayOfWeek()) {
-				dataAnticipo = dataAnticipo2;
-			}
-		} else if (dataAnticipo1 == null && dataAnticipo2 != null) {
-			dataAnticipo = dataAnticipo2;
-		}
-		LocalDateTime dataGiornata = giornataInfo.getDataGiornata();
-		LocalDateTime dataPosticipo = giornataInfo.getDataPosticipo();
-
-		if (dataGiornata != null) {
-			currentDate = dataGiornata;
-
-			if (dataAnticipo != null) {
-				currentDate = dataAnticipo;
-				LOG.info("now.getDayOfWeek() : " + now.getDayOfWeek());
-				LOG.info("dataGiornata.getDayOfWeek() : " + dataGiornata.getDayOfWeek());
-				if ( now.isAfter(dataAnticipo) && now.getDayOfWeek() == dataGiornata.getDayOfWeek() ) {
-					currentDate = dataGiornata;
-				}
-			}
-			
-			if (dataPosticipo != null) {
-				LOG.info("now.getDayOfWeek() : " + now.getDayOfWeek());
-				LOG.info("dataPosticipo.getDayOfWeek() : " + dataPosticipo.getDayOfWeek());
-				if ( now.getDayOfWeek() == dataPosticipo.getDayOfWeek() ) {
-					currentDate = dataGiornata;
-				}
-			} else {
-				if (dataAnticipo != null) {
-					currentDate = dataAnticipo;	
-				}
-			}
-		}
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-		String currentDataGiornata = currentDate.format(formatter);
-
-		return currentDataGiornata;
 	}
 
 	private Date getCalendarCountDown(String currentDataGiornata,
@@ -214,42 +160,6 @@ public class AuthenticatedUser{
 		return c.getTime();
 	}
 
-	private long getMillisDiff(String nextDate, String fusoOrario)
-			throws Exception {
-
-		Calendar c = Calendar.getInstance();
-		DateFormat fmt = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		String currentDataGiornata = fmt.format(c.getTime());
-		// String strDate1 = "2018/12/17 19:00:00";
-		// String strDate2 = "2018/12/17 20:00:00";
-		String strDate1 = currentDataGiornata;
-		String strDate2 = nextDate;
-
-		fmt.setLenient(false);
-		Date d1 = fmt.parse(strDate1);
-		Date d2 = fmt.parse(strDate2);
-
-		// Calculates the difference in milliseconds.
-		long millisDiff = d2.getTime() - d1.getTime();
-		int seconds = (int) (millisDiff / 1000 % 60);
-		int minutes = (int) (millisDiff / 60000 % 60);
-		int hours = (int) (millisDiff / 3600000 % 24);
-		int days = (int) (millisDiff / 86400000);
-
-		LOG.info(days + " days, ");
-		LOG.info(hours + " hours, ");
-		LOG.info(minutes + " minutes, ");
-		LOG.info(seconds + " seconds");
-
-		long diffFuso = Long.parseLong(fusoOrario) * 3600000;
-		millisDiff = millisDiff - diffFuso;
-
-		if (millisDiff < 0) {
-			millisDiff = 0;
-		}
-
-		return millisDiff;
-	}
 
 	public String getType() {
 		return type;
