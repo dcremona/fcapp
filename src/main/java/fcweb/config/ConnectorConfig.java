d100b653d0feffcccd1800c8c0f8f1b6d83eadec
USERSAD\dcremona <davide.cremona@nttdata.com>
2024-09-29 20:20:02 +0200

    fox

--- src/main/java/fcweb/config/ConnectorConfig.java
+++ src/main/java/fcweb/config/ConnectorConfig.java
@@ -10,6 +10,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConnectorConfig {
 
+//	# Original port configuration for HTTP, now being used for HTTPS
+//	server.port=${PORT:8443}
+//	# Our own custom field (this isn't defined by Spring)
+//	server.http.port=${HTTP_PORT:14939}
+//	# Tell Spring Security (if used) to require requests over HTTPS
+//	security.require-ssl=true
+//	# The format used for the keystore 
+//	server.ssl.key-store-type=PKCS12
+//	# The path to the keystore containing the certificate
+//	server.ssl.key-store=classpath:keystore.p12
+//	# The password used to generate the certificate
+//	server.ssl.key-store-password=fclt2024
+//	# The alias mapped to the certificate
+//	server.ssl.key-alias=tomcat
+	
 //	@Value("${server.http.port}")
 //    private int SERVER_HTTP_PORT;
 //
@@ -23,7 +38,18 @@ public class ConnectorConfig {
 //
 //        return tomcat;
 //    }
-	    
+
+    
+    
+    
+//    #server.port=${PORT:8080}
+//    #server.port=8443
+//    #server.ssl.key-store-type=PKCS12
+//    #server.ssl.key-store=classpath:springboot.p12
+//    #server.ssl.key-store-password=fclt2024
+//    #server.ssl.key-alias=springboot
+//    #server.ssl.key-password=fclt2024
+    
 //	@Bean
 //	public TomcatServletWebServerFactory servletContainer() {
 //		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
